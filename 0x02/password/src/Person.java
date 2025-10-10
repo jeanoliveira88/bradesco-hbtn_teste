a1a8a4edc86c2d978a2f56a520fd1baf6e8b6d1b
Jean Oliveira <jean.valid@gmail.com>
2025-10-10 16:55:05 -0300

    add files

---
+++ 0x02/password/src/Person.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Person {
+
+    public boolean checkUser(String username) {
+        if (username.length() < 8) return false;
+        return username.matches("^[a-zA-Z0-9]+$");
+    }
+
+    public boolean checkPassword(String password) {
+        if (password.length() < 8) return false;
+        boolean hasUppercase = password.matches(".*[A-Z].*");
+        boolean hasDigit = password.matches(".*\\d.*");
+        boolean hasSpecial = password.matches(".*[^a-zA-Z0-9].*");
+
+        return hasUppercase && hasDigit && hasSpecial;
+    }
+}
